9a92c4d1daefffa9d9e0acf09b405febe1ef8153
scott-korin <wydok42@gmail.com>
2022-05-10 23:33:51 -0400

    Cleanup.

--- KarateChop-Imp1/KarateChop-Imp1/Program.cs
+++ KarateChop-Imp1/KarateChop-Imp1/Program.cs
@@ -1,40 +1,50 @@
 ﻿using Search;
 using System.Diagnostics;
 
-int[] evenArray = new int[1000000];
-for (int i = 0; i < evenArray.Length; i++)
+
+namespace SearchConsoleApp
 {
-	evenArray[i] = i * 2;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] evenArray = new int[40000000];
+            for (int i = 0; i < evenArray.Length; i++)
+            {
+                evenArray[i] = (i + 1) * 2;
+            }
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            BinarySearch search = new BinarySearch();
+            int index = search.Chop(80000000, evenArray);
+            watch.Stop();
+
+            Console.WriteLine("Binary Search for last item in array of {0}, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
+
+
+            watch.Start();
+            search = new BinarySearch();
+            index = search.Chop(1, evenArray);
+            watch.Stop();
+
+            Console.WriteLine("Binary Search for item not in array of {0} items, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
+
+
+            watch.Start();
+            BruteForceSearch bruteSearch = new BruteForceSearch();
+            index = bruteSearch.Search(80000000, evenArray);
+            watch.Stop();
+
+            Console.WriteLine("Brute Force Search for last item in array of {0}, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
+
+
+            watch.Start();
+            bruteSearch = new BruteForceSearch();
+            index = bruteSearch.Search(1, evenArray);
+            watch.Stop();
+
+            Console.WriteLine("Brute Force Search for item not in array of {0} items, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
+        }
+    }
 }
-
-Stopwatch watch = new Stopwatch();
-watch.Start();
-BinarySearch search = new BinarySearch();
-int index = search.Chop(1999998, evenArray);
-watch.Stop();
-
-Console.WriteLine("Binary Search for last item in array of {0}, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
-
-
-watch.Start();
-search = new BinarySearch();
-index = search.Chop(1, evenArray);
-watch.Stop();
-
-Console.WriteLine("Binary Search for item not in array of {0} items, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
-
-
-watch.Start();
-BruteForceSearch bruteSearch = new BruteForceSearch();
-index = bruteSearch.Search(1999998, evenArray);
-watch.Stop();
-
-Console.WriteLine("Brute Force Search for last item in array of {0}, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
-
-
-watch.Start();
-bruteSearch = new BruteForceSearch();
-index = bruteSearch.Search(1, evenArray);
-watch.Stop();
-
-Console.WriteLine("Brute Force Search for item not in array of {0} items, Index is {1}.  Ellapsed time: {2}ms", evenArray.Length, index, watch.ElapsedMilliseconds);
--- KarateChop-Imp1/Search/BinarySearch.cs
+++ KarateChop-Imp1/Search/BinarySearch.cs
@@ -7,32 +7,21 @@
 			if (sortedArray == null || sortedArray.Length == 0)
 				return -1;
 
-			if (sortedArray.Length == 1)
-				if (sortedArray[0] == query)
-					return 0;
-				else
-					return -1;
-
 			return Recurse(query, sortedArray, 0, sortedArray.Length - 1);
-
 		}
 
 		private static int Recurse(int query, int[] sortedArray, int min, int max)
 		{
 			if (min > max)
-			{
 				return -1;
-			}
+
+			int middleIndex = (min + max) / 2;
+			if (sortedArray[middleIndex] == query)
+				return middleIndex;
+			else if (sortedArray[middleIndex] > query)
+				return Recurse(query, sortedArray, min, middleIndex - 1);
 			else
-			{
-				int middleIndex = (min + max) / 2;
-				if (sortedArray[middleIndex] == query)
-					return middleIndex;
-				else if (sortedArray[middleIndex] > query)
-					return Recurse(query, sortedArray, min, middleIndex - 1);
-				else
-					return Recurse(query, sortedArray, middleIndex + 1, max);
-			}
+				return Recurse(query, sortedArray, middleIndex + 1, max);
 		}
 	}
 }
